cf9d6451333c58b1d84b8a23ef66dd51e904de59
sarancse2010 <sarancse2010@gmail.com>
2026-01-24 13:58:50 +0530

    Add files via upload
    commit all files

---
+++ 02-get-started.js
@@ -0,0 +1,32 @@
+//var x;
+//var x;
+//console.log(x);
+
+//var x=100;
+//var x=101;
+//console.log(x);
+
+//let y;
+//let y;
+//console.log(y);
+//let y=100;
+//let y=101;
+//console.log(y);
+
+//const z;
+//const z;
+//console.log(z);
+//const z=100;
+//const z= 101;
+//console.log(z);
+
+//let hoisting - will
+//console.log(y);
+//let y
+//let y=101;
+//console.log(y);
+//const z
+//console.log(z)// throws error
+console.log(x)
+var x=10
+console.log(x);
